3e056ffca6ab7db7a6ff7aafbdb85cd74460d89d
Felipe <Felipe@DESDOBLEP6>
2017-10-17 16:37:54 +0200

    Contar pares

--- pares/src/pares/Pares.java
+++ pares/src/pares/Pares.java
@@ -4,12 +4,18 @@ public class Pares
 {
     public static void main (String[] args)
     {
-        System.out.println("Entre el 1 y el 10 hay "+contarPares(1,10));
+        System.out.println("Entre el 1 y el 10 hay "+contarPares(1,10)+" números pares");
     }
 private static int contarPares(int desde, int hasta)
 {
     int numPares=0;
-    
+    mover();
+    if (true)
+        return 0;   
     return numPares;
+}
+private static void mover()
+{
+    
 }
 }
