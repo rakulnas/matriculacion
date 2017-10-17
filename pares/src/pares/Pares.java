1e925d3de0e5512d1ed95904c1a1c384341eab18
Felipe <Felipe@DESDOBLEP6>
2017-10-17 16:25:01 +0200

    v2

---
+++ pares/src/pares/Pares.java
@@ -0,0 +1,15 @@
+
+package pares;
+public class Pares
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Entre el 1 y el 10 hay "+contarPares(1,10));
+    }
+private static int contarPares(int desde, int hasta)
+{
+    int numPares=0;
+    
+    return numPares;
+}
+}
